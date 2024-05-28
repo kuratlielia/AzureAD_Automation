75919cba94fb133c38a6911b17626352d6ccb9f8
Elia Kuratli <82137127+kuratlielia@users.noreply.github.com>
2024-05-28 22:58:22 +0200

    Delete secure.ps1

--- secure.ps1
+++
@@ -1,4 +0,0 @@
-﻿$username = "eliakuratli@solonweb.onmicrosoft.com"
-$password = "SXfyXfCy&noci5Lc" | ConvertTo-SecureString -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential($username, $password)
-$credential | Export-Clixml -Path "C:\temp\credentials.xml"
